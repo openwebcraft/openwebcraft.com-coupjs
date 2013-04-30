e1b00f4ef456faab0b5a7273fab6be7f19832272
Matthias Geisler <matthias@semanticdreamer.com>
2013-04-30 23:28:05 +0200

    render icon for post|draft.attachments in list views

--- views/recent-posts/map.js
+++ views/recent-posts/map.js
@@ -2,7 +2,16 @@ function(doc) {
     if (doc.created_at && doc.type == "post" && doc.status == "published") {
         var _ = require('views/lib/underscore');
         var moment = require('views/lib/moment');
-        emit(doc.created_at, _.extend({created_at_from_now: moment(doc.created_at).fromNow()}, doc));
+        emit(doc.created_at, _.extend(
+            {
+                // human readbale created_at date using moment.js
+                created_at_from_now: moment(doc.created_at).fromNow(),
+                // flatten out _attachments to ease {{ mustache }} rendering
+                attachments: _.map(doc._attachments, function(value, key, list) {
+                    return {filename: key, metadata: value};
+                })
+            }, doc)
+        );
         //emit(doc.created_at, null);
     }
 }
